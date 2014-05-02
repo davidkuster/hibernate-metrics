b7f680c86d63be789759a3dcfc979d6471be10ab
David Kuster <dave@choicetrax.com>
2014-05-01 21:06:31 -0700

    upgraded release plugin version

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -32,7 +32,7 @@ grails.project.dependency.resolution = {
         runtime ":hibernate:$grailsVersion"
 
         build(":tomcat:$grailsVersion",
-              ":release:1.0.0") {
+              ":release:2.0.2") {
             export = false
         }
 
