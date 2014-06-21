37c8e059698302d159150bd0d9434b7f2b310315
David Kuster <dave@choicetrax.com>
2014-06-21 10:46:13 -0700

    updated display so stats are before code results

--- src/groovy/net/talldave/hibernatemetrics/HibernateMetrics.groovy
+++ src/groovy/net/talldave/hibernatemetrics/HibernateMetrics.groovy
@@ -23,23 +23,46 @@ class HibernateMetrics {
     metricsService.clearStats()
     metricsService.markStartTime()
 
-    c.call()
+    def codeResults = c.call()
 
     metricsService.markActionEndTime()
     metricsService.markViewEndTime()
     metricsService.disableMetrics()
 
-    def results = metricsService.getMetricsMap()
+    def metricsResults = metricsService.getMetricsMap()
 
-    displayResults(results)
+    displayMetricsResults(metricsResults)
+    displayCodeResults(codeResults)
   }
 
 
-  static private void displayResults(Map results) {
-    println "\nTime Metrics: "
-    results['Time Metrics'].each { println it }
-    println "\nDB Metrics: "
-    results['DB Metrics'].each { println it }
+  static private void displayMetricsResults(Map results) {
+    println """
+-------------------------------------
+
+Time Metrics:
+${results['Time Metrics'].collect { k, v -> "  $k = $v" }.join('\n') }
+
+DB Metrics:
+${results['DB Metrics'].collect { k, v ->
+  if ( v in Map ) {
+    def map = v.collect { a, b ->
+      "\n    $a = $b"
+    }.join('')
+    "\n  $k = $map\n"
+  }
+  else {
+    "  $k = $v"
+  }
+}.join('\n') }
+
+-------------------------------------
+"""
+  }
+
+
+  static private void displayCodeResults(codeResults) {
+    println codeResults
   }
 
 }
