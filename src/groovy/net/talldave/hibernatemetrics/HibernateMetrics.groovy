0fc3906acf6a862749798b84562d54569f999415
David Kuster <dave@choicetrax.com>
2014-07-13 21:39:14 -0700

    reformatted code, added option to control which metrics are displayed via DatabaseMetricsType enum

--- src/groovy/net/talldave/hibernatemetrics/HibernateMetrics.groovy
+++ src/groovy/net/talldave/hibernatemetrics/HibernateMetrics.groovy
@@ -1,68 +1,93 @@
 package net.talldave.hibernatemetrics
 
+import static net.talldave.hibernatemetrics.DatabaseMetricsType.*
+
 import grails.util.Holders
+
 
 class HibernateMetrics {
 
-  static HibernateMetricsService metricsService
+    static HibernateMetricsService metricsService
 
 
-  static void initService() {
-    if ( ! metricsService ) {
-      metricsService = Holders.grailsApplication.mainContext.getBean('hibernateMetricsService')
+    static void initService() {
+        if ( ! metricsService ) {
+            metricsService = Holders.grailsApplication.mainContext.getBean('hibernateMetricsService')
+        }
     }
-  }
 
 
-  static void withSqlLogging( Closure c ) {
-    if ( ! c ) return
+    // apparently not possible to use varargs and inline closure param - bummer
+    //static void withSqlLogging( DatabaseMetricsType... types = [ALL], Closure c ) {
+    static void withSqlLogging( List<DatabaseMetricsType> types = [ALL], Closure c ) {
+        if ( ! c ) return
 
-    initService()
+        initService()
 
-    metricsService.enableMetrics()
-    metricsService.clearStats()
-    metricsService.markStartTime()
+        metricsService.enableMetrics()
+        metricsService.clearStats()
+        metricsService.markStartTime()
 
-    def codeResults = c.call()
+        def codeResults = c.call()
 
-    metricsService.markActionEndTime()
-    metricsService.markViewEndTime()
-    metricsService.disableMetrics()
+        metricsService.markActionEndTime()
+        metricsService.markViewEndTime()
+        metricsService.disableMetrics()
 
-    def metricsResults = metricsService.getMetricsMap()
+        def metricsResults = metricsService.getMetricsMap()
 
-    displayMetricsResults(metricsResults)
-    displayCodeResults(codeResults)
-  }
+        displayMetricsResults(metricsResults, types)
+        displayCodeResults(codeResults)
+    }
 
 
-  static private void displayMetricsResults(Map results) {
-    println """
--------------------------------------
+    static private void displayMetricsResults(Map results, List<DatabaseMetricsType> types) {
+        println """
+---------- Hibernate Metrics ----------
 
 Time Metrics:
-${results['Time Metrics'].collect { k, v -> "  $k = $v" }.join('\n') }
+${formatMetrics( results['Time Metrics'] )}
 
 DB Metrics:
-${results['DB Metrics'].collect { k, v ->
-  if ( v in Map ) {
-    def map = v.collect { a, b ->
-      "\n    $a = $b"
-    }.join('')
-    "\n  $k = $map\n"
-  }
-  else {
-    "  $k = $v"
-  }
-}.join('\n') }
+${formatMetrics( results['DB Metrics'], types )}
 
--------------------------------------
+---------------------------------------
 """
-  }
+    }
+
+    static private String formatMetrics( Map metrics, List types = null ) {
+        def typeNames = types*.toString()
+
+        metrics.collect { k, v ->
+            if ( typeNames && ! (typeNames =~ ALL.toString()) && ! (typeNames =~ k) ) {
+                // skip excluded metrics types
+                return
+            }
+            else if ( v in Map ) {
+                formatMetricsMap( k, v )
+            }
+            else {
+                "  $k = $v"
+            }
+        }.findAll { it }.join('\n')
+    }
 
 
-  static private void displayCodeResults(codeResults) {
-    println codeResults
-  }
+    static private String formatMetricsMap( String key, Map val, indent=2 ) {
+        def map = val.collect { a, b ->
+            if ( b in Map ) {
+                formatMetricsMap( a, b, indent + 2 )
+            }
+            else {
+                "\n${' ' * (indent + 2)}$a = $b"
+            }
+        }.join('')
+        "\n${' ' * indent}$key = $map"
+    }
+
+
+    static private void displayCodeResults(codeResults) {
+        println codeResults
+    }
 
 }
