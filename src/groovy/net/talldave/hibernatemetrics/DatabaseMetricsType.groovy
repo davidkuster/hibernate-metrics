7fdb947f26dce32e77ba1b74ed7ccc372f5429b1
David Kuster <dave@choicetrax.com>
2014-07-13 21:34:50 -0700

    added enum to detail grouped DB metrics types

---
+++ src/groovy/net/talldave/hibernatemetrics/DatabaseMetricsType.groovy
@@ -0,0 +1,26 @@
+package net.talldave.hibernatemetrics
+
+
+enum DatabaseMetricsType {
+
+    //ALL, QUERIES, DOMAINS, QUERY_CACHE, SECOND_LEVEL_CACHE, SESSIONS, MISC_COUNTS
+
+    ALL('All'),
+    COUNTS('Counts'),
+    SQL('SQL'),
+    DOMAINS('Domains'),
+    QUERY_CACHE('Query Cache'),
+    SECOND_LEVEL_CACHE('2L Cache'),
+    SESSIONS('Sessions')
+
+    private String displayVal
+
+    private DatabaseMetricsType(String val) {
+        this.displayVal = val
+    }
+
+    String toString() {
+        displayVal
+    }
+
+}
