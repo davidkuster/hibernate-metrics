dcc838f13d89aaf22171ceded5bdef67751d359c
David Kuster <dave@choicetrax.com>
2015-08-20 14:02:19 -0700

    bumped patch version

--- HibernateMetricsGrailsPlugin.groovy
+++ HibernateMetricsGrailsPlugin.groovy
@@ -2,7 +2,7 @@ import net.talldave.hibernatemetrics.HibernateMetrics
 
 class HibernateMetricsGrailsPlugin {
 
-    def version = '0.1.0'
+    def version = '0.1.1'
     def grailsVersion = '2.0 > *'
     def title = 'Hibernate Metrics'
     def author = 'David Kuster'
